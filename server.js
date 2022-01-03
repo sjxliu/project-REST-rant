9e179f078a81716cc3267fce2775a6021582474b
unknown <sjxliu8@gmail.com>
2022-01-03 16:40:00 -0600

    added home page view

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.set("view engine", "jsx")
+app.engine("jsx", require("express-react-views").createEngine())
+
 // import the control router
 app.use("/places", require("./controllers/places-control"))
 
 
 //index
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("home")
 });
 
 
