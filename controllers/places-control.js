be4902b1f2c268e1d3f61ea9f0c0907777945faa
unknown <sjxliu8@gmail.com>
2022-01-03 22:05:41 -0600

    added places page

--- controllers/places-control.js
+++ controllers/places-control.js
@@ -1,28 +1,29 @@
 const router = require("express").Router();
+const places = require("../models/places");
 
 // New above places the exact matching route needs to come first.
 router.get("/new", (req, res) => {
   res.render("/places/new");
 });
 
+//Post
+router.post("/", (req, res) => {
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body)
+  res.redirect("/places");
+});
+
 //places
 router.get("/places/:id", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "../images/matcha.jpg",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "../images/pizza.jpg",
-    },
-  ];
   res.render("/places/index", { places });
 });
 
