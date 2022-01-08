eb0da058e838c91d9eac88153365754063c2b4fa
unknown <sjxliu8@gmail.com>
2022-01-07 22:36:04 -0600

    pages showing added founded to new page

--- controllers/places-control.js
+++ controllers/places-control.js
@@ -1,111 +1,58 @@
 const router = require("express").Router();
+const db = require("../models");
 const places = require("../models/places");
-var bodyParser = require("body-parser");
 
-// create application/json parser
-//var jsonParser = bodyParser.json()
-
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-// New above places the exact matching route needs to come first.
-router.get("/new", (req, res) => {
-  res.render("/places/New");
-});
-
-//Places
 router.get("/", (req, res) => {
-  // let places = [
-  //   {
-  //     name: "H-Thai-ML",
-  //     city: "Seattle",
-  //     state: "WA",
-  //     cuisines: "Thai, Pan-Asian",
-  //     pic: "/images/thaiImage.jpg",
-  //   },
-  //   {
-  //     name: "Coding Cat Cafe",
-  //     city: "Phoenix",
-  //     state: "AZ",
-  //     cuisines: "Coffee, Bakery",
-  //     pic: "images/catCafeImage.jpg",
-  //   },
-  // ];
-  res.render("places/Index", { places });
-});
-
-//post
-router.post("/", urlencodedParser, (req, res) => {
-  const obj = JSON.parse(JSON.stringify(req.body));
-  console.log(obj);
-  res.send("POST /places");
-});
-
-//Show
-router.get("/:id", (req, res) => {
-  let id = Number(req.params.id);
-  if (isNaN(id)) {
-    res.render("error404");
-  } else if (!places[id]) {
-    res.render("error404");
-  } else {
-    res.render("places/show", {
-      place: places[req.params.id],
-      id: req.params.id,
+  db.Place.find()
+    .then((places) => {
+      res.render("places/index", { places });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("error404");
     });
-  }
 });
 
-//Edit
-router.get("/:id/edit", (req, res) => {
-  let id = Number(req.params.id);
-  if (isNaN(id)) {
-    res.render("error404");
-  } else if (!places[id]) {
-    res.render("error404");
-  } else {
-    res.render("places/edit", { place: places[id], id: req.params.id });
-  }
+//POST
+router.post("/", (req, res) => {
+  db.Place.create(req.body)
+    .then(() => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("error404");
+    });
 });
 
-//Update
+//NEW
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
+
+router.get("/:id", (req, res) => {
+  res.send("GET /places/:id stub");
+});
+
 router.put("/:id", (req, res) => {
-  let id = Number(req.params.id);
-  if (isNaN(id)) {
-    res.render("error404");
-  } else if (!places[id]) {
-    res.render("error404");
-  } else {
-    // Dig into req.body + make sure data is valid
-    if (!req.body.pic) {
-      //Default img if one not provided
-      req.body.pic = "http://placekitten.com/400/400";
-    }
-    if (!req.body.city) {
-      req.body.city = "Anytown";
-    }
-    if (!req.body.state) {
-      req.body.state = "USA";
-    }
-
-    console.log(req.body);
-    // Save the new data into places[id]
-    places[req.params.id] = req.body;
-    res.redirect(`/places/${id}`);
-  }
+  res.send("PUT /places/:id stub");
 });
 
-//Delete
+//DELETE
 router.delete("/:id", (req, res) => {
-  let id = Number(req.params.id);
-  if (isNaN(id)) {
-    res.render("error404");
-  } else if (!places[id]) {
-    res.render("error404");
-  } else {
-    places.splice(id, 1);
-    res.redirect("/places");
-  }
+  res.send("DELETE /places/:id stub");
+});
+
+router.get("/:id/edit", (req, res) => {
+  res.send("GET edit form stub");
+});
+
+router.post("/:id/rant", (req, res) => {
+  res.send("GET /places/:id/rant stub");
+});
+
+router.delete("/:id/rant/:rantId", (req, res) => {
+  res.send("GET /places/:id/rant/:rantId stub");
 });
 
 module.exports = router;
--- views/places/New.jsx
+++ views/places/New.jsx
@@ -9,7 +9,7 @@ function new_form() {
         <form method="POST" action="/places">
           <div className="form-group">
             <label htmlFor="name">Place Name</label>
-            <input className="form-control" id="name" name="name" required/>
+            <input className="form-control" id="name" name="name" required />
           </div>
           <div className="form-group">
             <label htmlFor="pic">Place Picture</label>
@@ -25,7 +25,16 @@ function new_form() {
           </div>
           <div className="form-group">
             <label htmlFor="cuisines">Cuisines</label>
-            <input className="form-control" id="cuisines" name="cuisines" required/>
+            <input
+              className="form-control"
+              id="cuisines"
+              name="cuisines"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label for="founded">Founded Year</label>
+            <input className="form-control" id="founded" name="founded" />
           </div>
           <input className="btn btn-primary" type="submit" value="Add Place" />
         </form>
