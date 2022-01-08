caa12b2b325ae906c78e9c59521dda34ba06c9bb
unknown <sjxliu8@gmail.com>
2022-01-08 09:13:42 -0600

    deployed

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
